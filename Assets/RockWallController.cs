fbeec72c42d3944ef30a09b32d269ee532a95ee3
Marc <marcus.denke@gmail.com>
2016-02-23 23:31:00 -0500

    ground update

--- Assets/RockWallController.cs
+++ Assets/RockWallController.cs
@@ -4,22 +4,35 @@ using System.Collections;
 public class RockWallController : SolidController {
 	public Transform Model;
 	private float intensity = .25f;
-
+	private float seed;
 	protected override void Start() {
 		base.Start ();
+
+		seed = 0.0f; //must get seed from place common to rock models..  Mathf.PI * 2.0f * UnityEngine.Random.value;
+
 		Mesh mesh = Model.GetComponent<MeshFilter>().mesh;
 		Vector3[] vertices = mesh.vertices;
 		Vector3[] normals = mesh.normals;
 		int i = 0;
+		float maxy = -9999.0f;
+		while (i < vertices.Length) {
+			if (vertices[i].y> maxy)
+				maxy = vertices[i].y;
+			i++;
+		}
+		i = 0;
 		while (i < vertices.Length){
-			var abspos = transform.position + vertices[i];
-			vertices[i] = vertices[i] + (new Vector3(0//Mathf.Sign (vertices[i].x)*Mathf.Sin (Mathf.Abs (abspos.x*abspos.y))
-			                                              ,Mathf.Sign (vertices[i].y)* Mathf.Sin (Mathf.Abs (abspos.x*abspos.z))
-			                                              , 0//Mathf.Sign(vertices[i].z)*Mathf.Sin (Mathf.Abs (abspos.z*abspos.y))
-			                                              )
-			                                  *.25f
-			            										
-			             					);
+			if(vertices[i].y == maxy){
+				var abspos = transform.position + vertices[i];
+				vertices[i] = vertices[i] + (new Vector3(		/*Mathf.Sign (vertices[i].x)**/Mathf.Sin (Mathf.Abs (abspos.x*abspos.y)+seed)
+				                                              ,	/*Mathf.Sign (vertices[i].y)**/Mathf.Sin (Mathf.Abs (abspos.x*abspos.z)+seed)
+				                                              , /*Mathf.Sign (vertices[i].z)**/Mathf.Sin (Mathf.Abs (abspos.z*abspos.y)+seed)
+				                                              )
+				                                  *intensity
+				            										
+				             					);
+
+			}
 			i++;
 		}
 		mesh.vertices = vertices;
